3fb946ff5bf35b922ef8c96e678c5df2b0ad51ce
Jens Thomae <j.thomae@outlook.com>
2023-10-21 21:59:49 +0200

    Added golang and tailwindcss language server to nvim.

--- nvchad/custom/configs/lspconfig.lua
+++ nvchad/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "pyright", "rust_analyzer", "gopls" }
+local servers = { "html", "cssls", "tsserver", "gopls", "clangd", "pyright", "rust_analyzer", "tailwindcss" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
