d5340263323d7a98488a945e9c2ebf0a537fa97b
Jens Thomae <jens.thomae@proton.me>
2023-12-13 00:07:12 +0100

    added vim defaults and removed nvim cmp escape shortcut

--- nvim/lua/core/defaults.lua
+++ nvim/lua/core/defaults.lua
@@ -1,2 +1,8 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.autoindent = true
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.scrolloff = 4
--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -41,7 +41,7 @@ return {
       				["<C-d>"] = cmp.mapping.scroll_docs(-4),
       				["<C-f>"] = cmp.mapping.scroll_docs(4),
       				["<C-Space>"] = cmp.mapping.complete(),
-      				["<Esc>"] = cmp.mapping.close(),
+      				-- ["<Esc>"] = cmp.mapping.close(),
       				["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
       				["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
 				['<CR>'] = cmp.mapping.confirm({ select = true }),
