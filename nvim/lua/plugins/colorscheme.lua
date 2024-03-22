eddc07441c59ea633cbad2056baea22d7e06cb8b
Jens Thomae <jens.thomae@proton.me>
2024-03-22 15:14:34 +0100

    Added some colorschemes to nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -15,4 +15,28 @@ return {
 	{
 		"NTBBloodbath/doom-one.nvim",
 	},
+    {
+        "nyoom-engineering/oxocarbon.nvim"
+    },
+    {
+        "bluz71/vim-moonfly-colors"
+    },
+    {
+        "joshdick/onedark.vim"
+    },
+    {
+        "folke/tokyonight.nvim"
+    },
+    {
+        "jacoborus/tender.vim"
+    },
+    {
+        "nanotech/jellybeans.vim"
+    },
+    {
+        "jaredgorski/SpaceCamp"
+    },
+    {
+        "NTBBloodbath/doom-one.nvim"
+    },
 }
