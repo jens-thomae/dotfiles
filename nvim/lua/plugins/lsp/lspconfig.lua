aa88eea0cbe8aebec0538d1ba8876baaa59d6a3e
Jens Thomae <jens.thomae@proton.me>
2024-04-04 13:14:15 +0200

    Added angularlsp to mason lspconfig

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -127,5 +127,10 @@ return {
 			capabilities = capabilities,
 			on_attach = on_attach,
 		})
+
+		lspconfig["angularls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 	end
 }
